75c909bfdbadd1eb9147a54ff63ef9208cf9dd58
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-06 10:20:09 -0700

    add findContact and deleteContact methods

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,26 @@ AddressBook.prototype.assignId = function(contact) {
 
 AddressBook.prototype.findContact = function(id) {
   for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
     if (this.contacts[i].id == id) {
       return this.contacts[i];
     }
+   }
   };
   return false;
 }
 
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i<this.contacts.length; i++) {    
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id){
+      delete this.contacts[i];
+      return true;
+      }
+     }
+    };
+    return false;
+  }
 
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
