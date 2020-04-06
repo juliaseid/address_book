594f171a5f7203094c1f8cc0004ebf67a2510c6c
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-06 09:55:21 -0700

    add code to assign ids to contacts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function(contact) {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      return this.contacts[i];
+    }
+  };
+  return false;
+}
+
+
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -8,4 +35,9 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-var testContact = new Contact("Ada", "Lovelace", "503-555-1111");
+
+var addressBook = new AddressBook();
+var contact = new Contact("Ada", "Lovelace", "503-555-0100");
+var contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
