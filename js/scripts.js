f431137bce4019226b6ce9096d41c8471760cce5
Julia Seidman <juliaseid@gmail.com>
2020-04-07 11:17:59 -0700

    Add email & street address functionality

--- js/scripts.js
+++ js/scripts.js
@@ -38,14 +38,21 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, email, street, city) {
   this.firstName = firstName,
   this.lastName = lastName,
-  this.phoneNumber = phoneNumber
+  this.phoneNumber = phoneNumber,
+  this.email = email,
+  this.street = street,
+  this.city = city
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.streetAddress = function() {
+  return this.street + ", " + this.city;
 }
 
 // User Interface Logic ---------
@@ -66,6 +73,9 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".email").html(contact.email);
+  $(".street-address").html(contact.street);
+  $(".city-zip").html(contact.city);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
@@ -89,10 +99,16 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedEmail = $("input#new-email").val();
+    var inputtedStreetAddress = $("input#new-street").val();
+    var inputtedCityZip = $("input#new-city").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    $("input#new-email").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedStreetAddress, inputtedCityZip);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   })
